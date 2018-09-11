9592dafb2581ba59da065a8b4c850a193bd2de06
Sofyan Saputra <meongbego@gmail.com>
2018-09-12 02:28:23 +0700

    Fix: Kubernetes Repo Bugs

--- run.sh
+++ run.sh
@@ -72,14 +72,14 @@ function install_kubernetes(){
     check_inet
     echo "5. Installing Kubernetes"
     cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-    [kubernetes]
-    name=Kubernetes
-    baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-    enabled=1
-    gpgcheck=1
-    repo_gpgcheck=1
-    gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
     yum install -y kubelet kubeadm kubectl
 }
